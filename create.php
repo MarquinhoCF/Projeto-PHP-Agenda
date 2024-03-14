b5adafa23418a57ee389897422c3145554dc2132
MarquinhoCF <marcos13@gmail.com>
2024-03-14 11:18:04 -0300

    Criando tela de adicionar contato

--- create.php
+++ create.php
@@ -4,8 +4,27 @@
 
 ?>
 
-    <h1>Criando Contato</h1>
-    <i class="fas fa-eye"></i>
+    <div class="container">
+        <?php include_once("templates/backbtn.html") ?>
+        <h1 id="main-title">Criar Contato</h1>
+        <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
+            <input type="hidden" name="type" value="create">
+            <div class="form-group">
+                <label for="name">Nome do Contato:</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
+            </div>
+            <div class="form-group">
+                <label for="phone">Telefone do contato:</label>
+                <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" required>
+            </div>
+            <div class="form-group">
+                <label for="observations">Observações:</label>
+                <textarea type="text" class="form-control" id="observations" name="observations" 
+                placeholder="Insira as observações" rows="3"></textarea>
+            </div>
+            <button id="btn-submit" type="submit" class="btn btn-primary">Cadastrar</button>
+        </form>
+    </div>
 
 <?php 
 
