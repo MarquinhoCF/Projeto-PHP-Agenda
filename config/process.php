c618d8c77d0ef26c38fea2eb1a3f4a4d1f5399cb
MarquinhoCF <marcos13@gmail.com>
2024-03-14 11:51:41 -0300

    Funcionalidade de editar contatos

--- config/process.php
+++ config/process.php
@@ -30,6 +30,29 @@
                 $error = $e->getMessage();
                 echo "Erro: $error";
             }
+        } else if ($data["type"] === "edit") {
+            $name = $data["name"];
+            $phone = $data["phone"];
+            $observations = $data["observations"];
+            $id = $data["id"];
+
+            $query = "UPDATE contacts 
+                      SET name = :name, phone = :phone, observations = :observations 
+                      WHERE id = :id";
+            $stmt = $conn->prepare($query);
+            $stmt->bindParam(":name", $name);
+            $stmt->bindParam(":phone", $phone);
+            $stmt->bindParam(":observations", $observations);
+            $stmt->bindParam(":id", $id);
+
+            try {
+                $stmt->execute();
+                $_SESSION['msg'] = "Contato atualizado com sucesso!";
+            } catch (PDOException $e) {
+                // erro
+                $error = $e->getMessage();
+                echo "Erro: $error";
+            }
         }
 
         // Redirect Home
