d7b43aeeb8e35d2af5979dc8ec7631cd53031cb9
MarquinhoCF <marcos13@gmail.com>
2024-03-14 11:46:14 -0300

    Criando formulário de edição

--- templates/header.php
+++ templates/header.php
@@ -33,7 +33,7 @@
             <div>
                 <div class="navbar-nav">
                     <a href="<?= $BASE_URL ?>index.php" class="nav-link active" id="home-link">Agenda</a>
-                    <a href="<?= $BASE_URL ?>create.php" class="nav-link active" id="home-link">Adicionar Contato</a>
+                    <a href="<?= $BASE_URL ?>create.php" class="nav-link active">Adicionar Contato</a>
                 </div>
             </div>
         </nav>
